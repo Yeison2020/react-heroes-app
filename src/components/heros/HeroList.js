fb6fbcb46c3ff208f8896797127382c6390b16be
Yeison <enmanuelquiterios@gmail.com>
2021-12-01 20:32:34 -0500

    Adding Style

--- src/components/heros/HeroList.js
+++ src/components/heros/HeroList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import getHeroesByPublisher from "../../selectors/getHeroesByPublisher";
 import HeroCard from "./HeroCard";
+import "./HeroList.css";
 
 const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
@@ -8,7 +9,7 @@ const HeroList = ({ publisher }) => {
   // Important note is how I'm passing the values using destruring with ppoprs
 
   return (
-    <div className="card-deck">
+    <div className="card-group">
       <h1>{publisher}</h1>
       <ul>
         {heroes.map((data) => {
